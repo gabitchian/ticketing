00bf67604be39da712a421149ba0009776a473ff
Gabriela Tchian <gftchian0609@gmail.com>
2020-05-20 14:30:34 -0300

    client

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,14 +1,24 @@
 import request from 'supertest';
 import {app} from '../../app';
+import {signupRouter} from '../signup';
 
 it('responds with details about the current user', async () => {
-  await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
+  const cookie = await global.signup();
 
-  return request(app).get('api/users/currentuser').expect(200);
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authnticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,59 @@
+import {useState} from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h3>Sign Up</h3>
+      <div className='form-group'>
+        <label>Email Address:</label>
+        <input
+          //   type='email'
+          className='form-control'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h5>Oops...</h5>
+          <ul className='my-0'>
+            {errors.map((err) => (
+              <li key={err.message}> {err.message} </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className='btn btn-primary'>Sign Up</button>
+    </form>
+  );
+};
