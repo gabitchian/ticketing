96af743c354283fff94050f75b37fc751a4e4090
Gabriela Tchian <gftchian0609@gmail.com>
2020-05-21 14:30:02 -0300

    client

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,24 +1,24 @@
 import {useState} from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const {doRequest, errors} = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    await doRequest();
   };
 
   return (
@@ -43,16 +43,7 @@ export default () => {
         />
       </div>
 
-      {errors.length > 0 && (
-        <div className='alert alert-danger'>
-          <h5>Oops...</h5>
-          <ul className='my-0'>
-            {errors.map((err) => (
-              <li key={err.message}> {err.message} </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
   return <h1>OIEEEEE</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
