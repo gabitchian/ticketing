219216ab28b500cfc3cdba15bbb7529d5c591ace
Gabriela Tchian <gftchian0609@gmail.com>
2020-05-28 15:16:43 -0300

    tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,11 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
-import {errorHandler, NotFoundError} from '@gabiticketing/common';
-import {currentUserRouter} from './routes/current-user';
-import {signinRouter} from './routes/signin';
-import {signoutRouter} from './routes/signout';
-import {signupRouter} from './routes/signup';
+import {errorHandler, NotFoundError, currentUser} from '@gabiticketing/common';
+import {createTicketRouter} from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,10 +15,9 @@ app.use(
   })
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
